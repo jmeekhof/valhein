1ac36f2435b53e3932a1e405a5a52f91ce230f5a
Josh Meekhof <jmeekhof@twotheleft.com>
2021-02-10 15:52:58 -0500

    Update to ubuntu 18.04

--- instance.tf
+++ instance.tf
@@ -16,8 +16,8 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 
   filter {
